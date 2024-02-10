aa743f3fe0d28a84907e5e1e3ad0710df7c1ef50
ranr01 <ran.rubin@mail.huji.ac.il>
2024-02-10 17:31:13 -0500

    use auth_bp

--- stubmoneyflask.py
+++ stubmoneyflask.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__)
+from werkzeug.security import generate_password_hash
+
+from auth_bp import auth_bp_factory, login_required
+
 
 transactions = [
     {
@@ -29,16 +32,47 @@ accounts = [
     {"id": 4, "name": "Maya (Savings)", "balance": 10000.0},
 ]
 
+users = [
+    {"id": 0, "username": "ranr", "password": generate_password_hash("250402")},
+]
+
+
+def get_user_from_user_id(id):
+    user = [u for u in users if u["id"] == id]
+    if len(user) > 0:
+        return user[0]
+    return None
+
+
+def get_user_from_username(username):
+    user = [u for u in users if u["username"] == username]
+    if len(user) > 0:
+        return user[0]
+    return None
+
+
 class State:
     pass
 
 
+app = Flask(__name__)
+app.config.from_mapping(SECRET_KEY="dev")
+
+app.register_blueprint(
+    auth_bp_factory(
+        get_user_from_username, get_user_from_user_id, "index", "auth.login"
+    )
+)
+
+
 @app.route("/")
+@login_required
 def index():
     return render_template("index.html", accounts=accounts)
 
 
 @app.route("/<int:account_id>/new-transaction", methods=["GET"])
+@login_required
 def new_transaction(account_id):
     state = State()
     state.name_is_active = False
@@ -51,6 +85,7 @@ def new_transaction(account_id):
 
 
 @app.route("/<int:account_id>/new-transaction", methods=["POST"])
+@login_required
 def post_new_transaction(account_id):
     transactions.append(
         {
@@ -65,6 +100,7 @@ def post_new_transaction(account_id):
 
 
 @app.route("/<int:account_id>/transactions", methods=["GET"])
+@login_required
 def get_transactions(account_id):
     state = State()
     state.name_is_active = True
@@ -78,6 +114,7 @@ def get_transactions(account_id):
 
 
 @app.route("/<int:account_id>/settings", methods=["GET"])
+@login_required
 def settings(account_id):
     state = State()
     state.name_is_active = False
@@ -90,5 +127,6 @@ def settings(account_id):
 
 
 @app.route("/<int:account_id>/settings", methods=["POST"])
+@login_required
 def post_settings(account_id):
     return redirect(url_for("index"))
