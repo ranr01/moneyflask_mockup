2091dde79f05c306957a1a9f83196692038ae164
ranr01 <ran.rubin@mail.huji.ac.il>
2024-02-10 17:30:36 -0500

    add auth_bp and  login page

---
+++ auth_bp.py
@@ -0,0 +1,70 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash
+
+
+def auth_bp_factory(
+        get_user_from_db_by_username,
+        get_user_from_db_by_user_id,
+        post_login_route,
+        post_logout_route,
+    ):
+    bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+    @bp.route('/login', methods=('GET', 'POST'))
+    def login():
+        if request.method == 'POST':
+            username = request.form['username']
+            password = request.form['password']
+            error = None
+            user = get_user_from_db_by_username(username)
+
+            if user is None:
+                error = 'Incorrect username.'
+            elif not check_password_hash(user['password'], password):
+                error = 'Incorrect password.'
+
+            if error is None:
+                session.clear()
+                session['user_id'] = user['id']
+                return redirect(url_for(post_login_route))
+
+            flash(error)
+
+        return render_template('auth/login.html')
+
+    @bp.before_app_request
+    def load_logged_in_user():
+        user_id = session.get('user_id')
+
+        if user_id is None:
+            g.user = None
+        else:
+            g.user = get_user_from_db_by_user_id(user_id)
+
+    @bp.route('/logout')
+    def logout():
+        session.clear()
+        return redirect(url_for(post_logout_route))
+
+    return bp
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+
+
+
